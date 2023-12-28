203d264a649084f0135f2c0453b1ea24a943c392
yashharne <yashharne.ed@gmail.com>
2023-12-28 22:56:49 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "unsplash.com",
+      },
+      {
+        hostname: "picsum.photos",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
