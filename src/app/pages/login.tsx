96bc2f5c24b1cccf2eaae441fb038979f03c732b
yashharne <yashharne.ed@gmail.com>
2024-04-10 21:04:37 +0530

    Update login page

--- src/app/pages/login.tsx
+++ src/app/pages/login.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import Image from "next/image";
+
 export default function Login() {
 
     return (
-        <div className="flex items-center justify-center w-screen h-screen">
+        <div className="flex flex-col items-center justify-center w-screen h-screen">
+            <Image src="/logo.png" alt="Application Logo" width={300} height={300} />
             <div className="btn btn-outline">
                 <a href="/api/auth/login"><h1>Login with SSO</h1></a>
             </div>
